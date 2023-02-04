4959c78939f745615045641a625ee0ceddb453cb
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2023-02-03 22:31:59 -0500

    Add index.html and main.js, startto add divs

---
+++ main.js
@@ -0,0 +1,7 @@
+// Etch-a-Sketch
+
+// Create webpage with a 16x16 grid of square divs
+const div = document.createElement('div');
+div.className = 'grid-divs';
+div.innerText = 'Grid';
+document.container.appendChild(div);
