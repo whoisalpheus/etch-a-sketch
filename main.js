698811de854f7daae8a8425b79a19b4fe6227bba
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2023-04-07 21:37:15 -0400

    How to add element to html

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 // Etch-a-Sketch
 
 // Create webpage with a 16x16 grid of square divs
+// const div = document.createElement('div');
+// div.className = 'grid-divs';
+// div.innerText = 'Grid';
+// document.container.appendChild(div);
+
+// Use for loop to make 16 x 16 grid
+// while (n > 1 || n < 16)
+
+// function createGrid(x){
+//     for (i = 0; i < x; i++) {
+//         for (j= 0; j < x; j++) {
+//             return '#';
+//         }
+//         return '#';
+// }} console.log(createGrid(17));
+
+// This is practice adding divs and styling in js:
+
+const body = document.querySelector('body');
 const div = document.createElement('div');
-div.className = 'grid-divs';
-div.innerText = 'Grid';
-document.container.appendChild(div);
+
+// Styling
+div.textContent = 'Hello, World!';
+div.style.color = 'yellow';
+div.style.backgroundColor = 'blue';
+
+body.appendChild(div);
