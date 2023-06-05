b54b1233f7944096a71d2ea82478e57df92adee7
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2023-06-05 12:05:22 -0400

    Working on trying to create a grid

--- main.js
+++ main.js
@@ -1,25 +1,44 @@
 // Etch-a-Sketch
 
-const div = document.createElement('div');
-div.classList.add('container');
-document.body.appendChild(div);
+const container = document.querySelector('.container');
+// const gridContainer = document.querySelectorAll('.grid');
 
-// Create webpage with a 16x16 grid of square divs
-// Must go within container !!!
-// !!!!!
-const myGrid = document.createElement('div');
-myGrid.classList.add('squares');
-document.body.appendChild(myGrid);
-
+// const myGrid = document.createElement('div');
+// myGrid.classList.add('grid');
+// container.appendChild(myGrid);
 
 // Use for loop to make 16 x 16 grid
+
+// Method 1: in a function
 // while (n > 1 || n < 16)
 
-// function createGrid(x){
-//     for (i = 0; i < x; i++) {
-//         for (j= 0; j < x; j++) {
-//             return '#';
-//         }
-//         return '#';
-// }} console.log(createGrid(17));
+function createGrid(n){
+    // Rows
+    for (let i = 0; i < n; i++) {
+        // Columns
+        for (let j = 0; j < n; j++) {
+            const myGrid = document.createElement('div');
+            myGrid.classList.add('grid');
+            container.appendChild(myGrid);
+        }
 
+}} createGrid(16);
+
+// Method 2: simple for loop with no function
+// const squareCount = 16;
+
+// for (let i = 0; i < squareCount * squareCount; i++) {
+
+// }
+
+// Method 3: ???
+// for (let i = 0; i < 16; i++) {
+//         const myColumns = document.createElement('div');
+//         const myRows = document.createElement('div');
+//         for (let j= 0; j < 16; j++) {
+//                 myColumns.classList.add('column');
+//                 container.appendChild(myColumns);
+//         }
+//         myRows.classList.add('row');
+//         myColumns.appendChild(myRows);
+// }
